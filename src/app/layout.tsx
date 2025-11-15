214f0434eecb7ac7e5b3df1ccc6ddcbe22c799c5
Alejandro Fidanza <alejandro@northshoreautomation.com>
2025-11-14 22:25:40 -0500

    add theme provider

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,8 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import Link from "next/link";
-import { homePath, ticketsPath } from "@/app/paths";
-import { buttonVariants } from "@/components/ui/button";
-import { LucideKanban } from "lucide-react";
 import { Header } from "@/components/header";
+import { ThemeProvider } from "@/components/theme/theme-provider";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -28,10 +25,11 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html suppressHydrationWarning lang="en">
     <body
       className={`${geistSans.variable} ${geistMono.variable} antialiased`}
     >
+    <ThemeProvider>
     <Header/>
     <main className="
         min-h-screen flex-1
@@ -41,6 +39,7 @@ export default function RootLayout({
         flex flex-co
       ">
       {children}</main>
+    </ThemeProvider>
     </body>
     </html>
   );
--- src/components/header.tsx
+++ src/components/header.tsx
@@ -2,6 +2,7 @@ import Link from "next/link";
 import { homePath, ticketsPath } from "@/app/paths";
 import { buttonVariants } from "@/components/ui/button";
 import { LucideKanban } from "lucide-react";
+import { ThemeSwitcher } from "@/components/theme/theme-switcher";
 
 const Header = ()=>{
   return (<nav className="
@@ -9,7 +10,7 @@ const Header = ()=>{
         fixed left-0 right-0 top-0 z-20
         border-b bg-background/95 backdrop-blur
         w-full flex py-2.5 px-5 justify-between">
-    <div>
+    <div className="flex align-items gap-x-2">
       <Link href={homePath()} className={buttonVariants({variant: "ghost"})}>
         <LucideKanban/>
         <h1 className="ml-2 text-lg font-semibold">
@@ -17,7 +18,8 @@ const Header = ()=>{
         </h1>
       </Link>
     </div>
-    <div>
+    <div className="flex align-items gap-x-2">
+      <ThemeSwitcher/>
       <Link href={ticketsPath()} className={buttonVariants({variant: "default"})}>Tickets</Link>
     </div>
   </nav>)
