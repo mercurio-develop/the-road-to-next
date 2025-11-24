9f227c3453d657eeab998b689f1f95571fca5af6
Alejandro Fidanza <alejandro@northshoreautomation.com>
2025-11-24 00:31:54 -0500

    tickets api

--- src/features/ticket/queries/get-ticket.tsx
+++ src/features/ticket/queries/get-ticket.tsx
@@ -1,8 +1,9 @@
 import { prisma } from "@/lib/prisma";
 
 export const getTicket = async (ticketId: string) => {
-  return (await prisma.ticket.findUnique({
+  console.log(ticketId)
+  return await prisma.ticket.findUnique({
     where: { id: ticketId },
     include: { user: { select: { username: true } } },
-  }));
+  });
 };
